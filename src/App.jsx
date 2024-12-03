ba7177f88778d241409d403b9d6ed3025a959bc4
jongho <whjk1104@gmail.com>
2024-12-03 16:00:03 +0900

    ✨ Mini Project Final
    Mini Project Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import './App.scss'
-import { useEffect } from "react"
+import { lazy, Suspense, useEffect } from "react"
 import { useDispatch } from 'react-redux';
 import { fetchMultiplePokemonById } from './RTK/thunk';
-import { Routes } from 'react-router-dom';
+import { Routes, useNavigate } from 'react-router-dom';
 import { Route } from 'react-router-dom';
-import Main from './pages/Main';
-import Detail from './pages/Detail';
-import Search from './pages/Search';
-import Favorite from './pages/Favorite';
 import { Link } from 'react-router-dom';
+
+const Main = lazy(() => import('./pages/Main'))
+const Detail = lazy(() => import('./pages/Detail'))
+const Search = lazy(() => import('./pages/Search'))
+const Favorite = lazy(() => import('./pages/Favorite'))
 
 
 function App() {
-
+  const navigate = useNavigate()
   const ditpatch = useDispatch()
   useEffect(() => {
     ditpatch(fetchMultiplePokemonById(151))
@@ -20,20 +21,26 @@ function App() {
 
   return (
     <>
-      <h1 className='text-[40px] text-center'>포켓몬 도감</h1>
-      <nav className='flex gap-[10px] justify-center'>
+      <h1 className='border-t-[30px] border-t-[red] bg-black text-white text-[40px] text-center'>포켓몬 도감</h1>
+      <nav className='py-[10px] border-b-[3px] border-b-black flex gap-[20px] justify-center'>
         <Link to={'/'}>메인</Link>
-        <Link to={'/detail/:pokemon'}>상세정보</Link>
-        <Link to={'/search'}>검색</Link>
         <Link to={'/favorite'}>찜목록</Link>
+        <div>
+        <input onChange={(e) => navigate(`/search?pokemon=${e.target.value}`)} 
+        className="w-[120px] border-b border-[darkgray] justify-center px-[20px]"
+        />
+        <span>🔎</span>
+        </div>
       </nav>
-      <main className='flex justify-center flex-wrap gap-[20px] pt-[20px]'>
-      <Routes>
-        <Route path={'/'} element={ <Main />} /> 
-        <Route path={'/detail/:pokemonId'} element={ <Detail />} /> 
-        <Route path={'/search'} element={ <Search />} /> 
-        <Route path={'/favorite'} element={ <Favorite />} /> 
-      </Routes>
+      <main className='pb-[20px] bg-[gray] flex justify-center flex-wrap gap-[20px] pt-[20px]'>
+        <Suspense fallback={<div>불러오는 중...</div>}>
+          <Routes>
+            <Route path={'/'} element={ <Main />} /> 
+            <Route path={'/detail/:pokemonId'} element={ <Detail />} /> 
+            <Route path={'/search'} element={ <Search />} /> 
+            <Route path={'/favorite'} element={ <Favorite />} /> 
+          </Routes>
+        </Suspense>
       </main>
     </>
   )
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,15 +1,19 @@
 import { useParams } from "react-router-dom"
 import { useSelector } from 'react-redux';
 import { selectPokemonById } from "../RTK/selector";
+import FavoriteButton from "../component/FavoriteButton";
+import FlipCard from "../component/FlipCard";
 
 export default function Detail() {
   const { pokemonId } = useParams()
   const pokemon = useSelector(selectPokemonById(Number(pokemonId)))
   return (
-    <div className="flex flex-col justify-center items-center border-[gray] p-[30px] rounded-[10px]">
-      <div className="text-[30px] mb-[10px]">{pokemon.name}</div>
+    <div className="bg-white flex flex-col justify-center items-center py-[30px] px-[60px] rounded-[10px] border-b-[8px] border-r-[8px] border-black">
+      <div className="text-[30px] mb-[10px]">{pokemon.name}
+        <FavoriteButton pokemonId={Number(pokemonId)} />
+      </div>
       <div className="whitespace-pre-wrap text-center">{pokemon.description}</div>
-      <img className="w-[200px]" src={pokemon.front}></img>
+      <FlipCard front={pokemon.front} back={pokemon.back} />
     </div>
   )
 }
